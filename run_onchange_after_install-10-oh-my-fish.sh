9077dccf61e62ae28090faf0d0360a4912799d6e
Tim Pansino <timpansino@gmail.com>
2023-01-26 03:34:52 +0000

    Fix noninteractive setup

--- run_onchange_after_install-10-oh-my-fish.sh
+++ run_onchange_after_install-10-oh-my-fish.sh
@@ -5,5 +5,7 @@ if type -q omf
     omf update
 else
     # Install OMF
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install -o /tmp/omf-install.fish
+    fish /tmp/omf-install.fish --noninteractive
+    rm /tmp/omf-install.fish
 end
