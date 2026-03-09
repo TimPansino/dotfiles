3318fc0281a05427ed3bd34f36271ded0d3b09df
Tim Pansino <timpansino@gmail.com>
2026-03-09 20:52:02 +0000

    Fix syntax error

--- run_onchange_after_install-20-pre-commit.sh
+++ run_onchange_after_install-20-pre-commit.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env fish
 
-if test -e ./.pre-commit-config.yaml or test -e ./.pre-commit-config.yml
+if test -e ./.pre-commit-config.yaml; or test -e ./.pre-commit-config.yml
     if type -q pre-commit
         pre-commit install-hooks
     end
 else
     echo "No pre-commit config detected, skipping install."
-end
-
-if type -q omf 
-    # Update OMF
-    omf update
-else
-    # Install OMF
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install -o /tmp/omf-install.fish
-    fish /tmp/omf-install.fish --noninteractive
-    rm /tmp/omf-install.fish
 end
 
